263e4c8f1c6cfe3b98ed4844468282566b89981d
buddha <buddhatamang1000@gmail.com>
2022-04-04 14:55:14 +0545

    feat(section): Make progress bar work with Js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let counter1 = 0;
 let counter2 = 1;
+let bool = true;
 
 const sections = document.querySelectorAll("section");
+const progress = document.querySelector(".progress h2");
+const circles = document.querySelectorAll(".circle");
 
-window.addEventListener("wheel", (e) => {
-  const deltaY = e.deltaY > 0;
-  if (deltaY) {
-    counter1++;
-    counter2++;
-  } else {
-    counter1--;
-    counter2--;
-  }
+const progressCounter = () => {
+  progress.textContent = `${counter2}/${sections.length}`;
 
+  Array.from(circles).forEach((circle) => {
+    circle.style.backgroundColor = "transparent";
+  });
+  document.querySelector(`.circle-${counter2}`).style.backgroundColor = "#ddd";
+};
+
+const pageController = () => {
+  bool = true;
   if (counter1 === 5) {
     Array.from(sections).forEach((section) => {
       section.style.left = "0";
     });
     counter1 = 0;
     counter2 = 1;
-    return;
+    progressCounter();
+    bool = false;
   }
 
   if (counter1 === -1) {
@@ -31,12 +36,49 @@ window.addEventListener("wheel", (e) => {
     });
     counter1 = 4;
     counter2 = 5;
-    return;
+    progressCounter();
+    bool = false;
+  }
+  progressCounter();
+  return bool;
+};
+
+window.addEventListener("wheel", (e) => {
+  const deltaY = e.deltaY > 0;
+  if (deltaY) {
+    counter1++;
+    counter2++;
+  } else {
+    counter1--;
+    counter2--;
   }
 
-  document.querySelector(
-    `.section-${deltaY ? counter1 : counter2}`
-  ).style.left = `${deltaY ? "-100vw" : "0"}`;
+  pageController();
+  progressCounter();
 
-  console.log(counter1, counter2);
+  bool &&
+    (document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}`
+    ).style.left = `${deltaY ? "-100vw" : "0"}`);
+});
+
+document.querySelector(".left-btn").addEventListener("click", () => {
+  counter1--;
+  counter2--;
+  pageController() &&
+    (document.querySelector(`.section-${counter2}`).style.left = "0");
+});
+
+document.querySelector(".right-btn").addEventListener("click", () => {
+  counter1++;
+  counter2++;
+  pageController() &&
+    (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+});
+
+document.querySelector(".grapes-img").addEventListener("mouseover", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = "0.5";
+});
+document.querySelector(".grapes-img").addEventListener("mouseout", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = "1";
 });
